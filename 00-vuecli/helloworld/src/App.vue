f7cf668f030e67e9b78ad614c4f41aaba1d4dae0
kflange <zknsftk1msrk@gmail.com>
2019-03-08 00:15:55 +0900

    show task list

--- 00-vuecli/helloworld/src/App.vue
+++ 00-vuecli/helloworld/src/App.vue
@@ -1,29 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h2>task list</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done" />
+        {{ task.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
   components: {
-    HelloWorld
+    //HelloWorld
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  get tasks() {
+    return this.$store.state.tasks;
+  }
+}
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- 00-vuecli/helloworld/src/store.ts
+++ 00-vuecli/helloworld/src/store.ts
@@ -2,6 +2,21 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
-const store = new Vuex.Store({});
+const store = new Vuex.Store({
+  state: {
+    tasks: [
+      {
+        id: 1,
+        name: "buy milk",
+        done: false
+      },
+      {
+        id: 2,
+        name: "buy book",
+        done: true
+      }
+    ]
+  }
+});
 
 export default store;
