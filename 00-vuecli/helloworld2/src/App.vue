45f6467ff08a1071f22222202706f04acf4a4243
kflange <zknsftk1msrk@gmail.com>
2019-03-10 09:40:14 +0900

    show task list

--- 00-vuecli/helloworld2/src/App.vue
+++ 00-vuecli/helloworld2/src/App.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
+    <h2>task list</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input
+          type="checkbox"
+          v-bind:checked="task.done"
+        />
+        {{ task.name }}
+      </li>
+    </ul>
   </div>
 </template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class App extends Vue {
+
+  get tasks() {
+    return this.$store.state.tasks;
+  }
+
+}
+</script>
+
 <style lang="stylus">
 </style>
--- 00-vuecli/helloworld2/src/store.ts
+++ 00-vuecli/helloworld2/src/store.ts
@@ -5,8 +5,22 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    tasks: [
+      {
+        id: 1,
+        name: 'buy milk',
+        labelIDs: [1, 2],
+        done: false,
+      },
+      {
+        id: 2,
+        name: 'buy book',
+        labelIDs: [1, 3],
+        done: true,
+      },
+    ],
   },
+
   mutations: {
 
   },
